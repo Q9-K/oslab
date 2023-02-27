a853928e090a4e5df3e8347e9ce746cec2dc1634
蒋康 <20374319@buaa.edu.cn>
2023-02-27 17:55:35 +0800

    为什么search.sh添加失败

--- ray/sh_test2/search.sh
+++ ray/sh_test2/search.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 #First you can use grep (-n) to find the number of lines of string.
 #Then you can use awk to separate the answer.
+grep -n $2 $1 > result_tmp
+awk -F : '{print $1}' result_tmp > $3
+rm result_tmp
 

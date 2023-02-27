7f9a1d8793104501a3f38fe65eb8b72be9e71cad
蒋康 <20374319@buaa.edu.cn>
2023-02-27 16:02:47 +0800

    完成实验1，2

--- ray/sh_test1/changefile.sh
+++ ray/sh_test1/changefile.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 a=1
+name1="file"
+name2="newfile"
 while [ $a -le 100 ]
 do
 	if [ $a -gt 70 ]	   #if loop variable is greater than 70
 	then
-
+		rm -rf  "$name1$a"
 	elif [ $a -gt 40 ]         # else if loop variable is great than 40
 	then
-
+		mv "$name1$a" "$name2$a" 
 	fi
-	           #don't forget change the loop variable
+	((a++))       #don't forget change the loop variable
 done
